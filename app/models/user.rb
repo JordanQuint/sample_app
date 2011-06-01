e5c7c09bbed264e52fe81fb9b3bfca7a95fd7ac3
Jordan Quint <jordanquint@Caseys-MacBook-Pro.local>
2011-06-01 11:29:23 -0500

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password #indicating that it's a virtual attribute, not a stored one
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -21,4 +22,43 @@ class User < ActiveRecord::Base
                     :length => { :maximum => 50},
                     :format => { :with => email_regex},
                     :uniqueness => { :case_sensitive => false}
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6 .. 40}
+                       
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypt(submitted_password) == encrypted_password
+  end
+  
+  def User.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+    #If we don't return on either of those statements,
+    # we'll return nil here at the end. Oh, magical Ruby.
+  end
+  
+  private
+    def encrypt_password
+      self.salt = make_salt if new_record? #only store the salt if
+                                           # this is a new record
+      self.encrypted_password = encrypt(password)
+      #have to put self.encrypted_password so it doesn't
+      # create a local variable, and instead assigns it to
+      # the global variable of encrypted_password
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name "Jordan quint"
+  user.email "jordanquint@hotmail.com"
+  user.password "apples"
+  user.password_confirmation "apples"
+end
