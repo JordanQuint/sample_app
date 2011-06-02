51cfea1e9fe044579d2bf188bb7d20b64a700833
Jordan Quint <jordanquint@hotmail.com>
2011-06-02 13:52:14 -0500

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -48,5 +48,58 @@ describe UsersController do
       #the :class attribute tests the CSS class's name
     end
   end
+  
+  describe "POST create" do
+    
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+                  :password_confirmation => ""}
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do
+      before(:each) do
+        @attr = { :name => "New User", :email => "user@example.com",
+                  :password => "apples", :password_confirmation => "apples"}
+      end
+      
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        # Note: =~ is a comparator for strings to regexs (regular expressions)
+        # For practice with regexs, you can visit Rubular online
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+      
+    end
+ 
+  end
 
 end
